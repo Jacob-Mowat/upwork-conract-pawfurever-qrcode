e9284dd91578be216fd100cac438fd70f4d41155
Jacob Mowat <zaxquit@protonmail.com>
2023-03-30 22:30:46 +0100

    Added @clerk authentication

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,15 @@
+import {
+  ClerkProvider,
+} from "@clerk/nextjs/app-beta";
 import './globals.css'
 
+// TODO: Not sure if this is needed anymore.
 export const metadata = {
   title: 'Dog QR Tagger',
   description: 'Next.js app for tagging dogs with QR codes',
 }
+
+
 
 export default function RootLayout({
   children,
@@ -11,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
