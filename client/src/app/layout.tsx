5d81c571b2e719f6ffddcab4906f4a7c95f9a9bb
Jacob Mowat <zaxquit@protonmail.com>
2023-03-31 02:34:32 +0100

    Added header to all pages

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import {
   ClerkProvider,
 } from "@clerk/nextjs/app-beta";
+import { UserButton } from "@clerk/nextjs/app-beta/client";
 import './globals.css'
 
 // TODO: Not sure if this is needed anymore.
@@ -20,6 +21,18 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
+          <div className="flex flex-row w-full h-16 bg-primary items-center border-b-2">
+            <div className="flex-none px-8">
+              <img src="https://cdn.shopify.com/s/files/1/0584/8714/4505/files/PawFurEver-03_125x.png?v=1650435807" width="64" height="64" alt="PawFurEver" />
+            </div>
+
+            <div className="grow text-center text-2xl">
+              Paw Fur Ever - Dog QR Tagger
+            </div>
+            <div className="flex-none px-8">
+              <UserButton />
+            </div>
+          </div>
           {children}
         </body>
       </html>
