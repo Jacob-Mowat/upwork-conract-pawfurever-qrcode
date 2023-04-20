bcefdadf37b232fc9d61504c850da4d701c453a1
Jacob Mowat <zaxquit@protonmail.com>
2023-04-20 16:08:43 +0100

    Added API route to create owner details

--- client/src/app/api/owner-details/route.ts
+++ client/src/app/api/owner-details/route.ts
@@ -20,3 +20,32 @@ export async function GET(request: Request) {
     //     }
     // });
 };
+
+export async function POST(request: Request) {
+    const { ownerDetails } = await request.json();
+
+    const newOwnerDetails = await prisma.owner_details.create({
+        data: {
+            owner_firstname: ownerDetails.owner_firstname,
+            owner_lastname: ownerDetails.owner_lastname,
+            owner_phone_number: ownerDetails.owner_phone_number,
+            owner_address_line1: ownerDetails.owner_address_line1,
+            owner_address_line2: ownerDetails.owner_address_line2,
+            owner_address_zip: ownerDetails.owner_address_zip,
+            owner: {
+                connect: {
+                    id: ownerDetails.owner.id
+                }
+            }
+        }
+    });
+
+    await prisma.$disconnect();
+
+    return NextResponse.json({
+        status:200,
+        body: {
+            ownerDetails: newOwnerDetails
+        }
+    });
+}
