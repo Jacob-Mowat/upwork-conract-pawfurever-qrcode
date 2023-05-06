f993867462ae474cde235389da804b17b614f1ab
Jacob Mowat <zaxquit@protonmail.com>
2023-05-06 20:19:07 +0100

    Removed unique constraint on setup_key

---
+++ client/prisma/migrations/20230506191833_removed_unique_constraint_on_setup_key/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "tag_setup_key";
+
+-- AlterTable
+ALTER TABLE "tags" ALTER COLUMN "setup_key" SET DEFAULT concat(concat(upper(LEFT(REGEXP_REPLACE(md5(random()::text),'[[:digit:]]','','g'), 3)), '-'), concat(concat(concat(floor(random() * (9-0+1) + 0)::int, floor(random() * (9-0+1) + 0)::int), floor(random() * (9-0+1) + 0)::int)),floor(random() * (9-0+1) + 0)::int);
