67dd8ae8836fb7e4ec6d7aa0604c895c47e40c81
Jacob Mowat <zaxquit@protonmail.com>
2023-05-11 13:02:57 +0100

    minor changes

--- client/src/app/api/tags/notifyOwnerByEmail/route.ts
+++ client/src/app/api/tags/notifyOwnerByEmail/route.ts
@@ -37,7 +37,7 @@ export async function GET(request: Request) {
     // Notify owner tag has been viewed/scanned
 
     const ENDPOINT = "https://send.api.mailtrap.io/";
-    const SENDER_EMAIL = "mailtrap@qr.mowat.dev";
+    const SENDER_EMAIL = process.env.MAILTRAP_SENDER_EMAIL as string;
 
     // Send email to owner to notify them the tag has been viewed/scanned.
     const client = new MailtrapClient({
@@ -45,7 +45,7 @@ export async function GET(request: Request) {
         token: process.env.NEXT_PUBLIC_MAILTRAP_API_TOKEN as string,
     });
 
-    const sender = { name: "Mailtrap Test", email: SENDER_EMAIL };
+    const sender = { name: "PawFurEver QR Tag", email: SENDER_EMAIL };
 
     if (tag_details?.parent_email == "" && tag_details?.parent_email_additional == "") {
         return NextResponse.json({
@@ -63,13 +63,13 @@ export async function GET(request: Request) {
             to: [{ email: tag_details?.parent_email as string }],
             subject: `${tag_details?.name} has been viewed/scanned!`,
             text: `
-                Hello ${tag_details?.name} has been viewed/scanned!
+                Hello ${tag_details?.name}'s qr tag has been viewed/scanned!
 
                 You can view the details of ${tag_details?.name} by clicking the link below:
-                ${process.env.DOMAIN_ADDRESS}/view/${tag?.TAG_TOKEN}
+                ${process.env.DOMAIN_ADDRESS}view/${tag?.TAG_TOKEN}
 
                 If you wish to edit the details of ${tag_details?.name} you can do so by clicking the link below:
-                ${process.env.DOMAIN_ADDRESS}/edit/${tag?.TAG_TOKEN}
+                ${process.env.DOMAIN_ADDRESS}edit/${tag?.TAG_TOKEN}
 
                 Thank you for using QR Pet Tags!
 
